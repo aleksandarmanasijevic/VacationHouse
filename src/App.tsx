0efd80c1559930c57d38801896f53a308197ce2f
Aleksandar Vukovic <soundform7@hotmail.com>
2020-11-29 19:12:24 +0100

    material UI added to the dependancy list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {NavigationBar} from './Components/General/NavigationBar/NavigationBar';
 import {Footer} from './Components/General/Footer/Footer';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
-  return (
+    const [isPageLoaded, setIsPageLoaded] = useState(false);
+
+    return (
     <div className="App">
       <NavigationBar/>
-
       <Footer/>
+      <CircularProgress disableShrink />  {/*Testing material ui*/}
     </div>
   );
 }
